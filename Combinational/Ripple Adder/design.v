fff4c40c9a96969465b7c3b3ae6ccc4f51c5e0d6
saisharan2k3 <saisharan2k3@gmail.com>
2026-02-16 10:17:34 +0530

    Update and rename design to design.v

--- Combinational/Ripple Adder/design.v
+++ Combinational/Ripple Adder/design.v
@@ -5,6 +5,7 @@ module full_adder (
     assign sum = a ^ b ^ cin;
     assign cout = (a & b) | (b & cin) | (a & cin);
 endmodule
+
 module RCA_4bit (
     input [3:0] a,      
     input [3:0] b,     
@@ -16,6 +17,6 @@ module RCA_4bit (
 full_adder fa0 (.a(a[0]), .b(b[0]), .cin(cin),  .sum(sum[0]), .cout(c1));
     full_adder fa1 (.a(a[1]), .b(b[1]), .cin(c1),   .sum(sum[1]), .cout(c2));
     full_adder fa2 (.a(a[2]), .b(b[2]), .cin(c2),   .sum(sum[2]), .cout(c3));
-    full_adder fa3 (.a(a[3]), .b(b[3]), .cin(c3),   .sum(sum[3]), .cout(cout));
+    full_adder fa3 (.a(a[3]), .b(b[3]), .cin(c3),   .sum(sum[3]),.cout(cout));
     
 endmodule
