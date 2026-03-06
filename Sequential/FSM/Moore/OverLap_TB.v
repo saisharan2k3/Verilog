46e0c2824ebf886267fdee68a547a2902b896480
saisharan2k3 <saisharan2k3@gmail.com>
2026-03-06 14:23:17 +0530

    Create OverLap_TB.v

---
+++ Sequential/FSM/Moore/OverLap_TB.v
@@ -0,0 +1,32 @@
+module tb;
+reg clk;
+reg reset;
+reg in;
+wire out;
+Moore_NonOverlap uut(.clk(clk),.reset(reset),.in(in),.out(out));
+always #5 clk=~clk;
+initial begin
+$monitor("time=%0t clk=%b reset=%b in=%b out=%b", $time,clk,reset,in,out);
+$dumpfile("Moore_Non.vcd");
+$dumpvars(0,tb);
+clk=0;
+reset=0;
+in=0;
+#10 reset=1;
+@(posedge clk)in=1;
+@(posedge clk)in=0;
+@(posedge clk)in=1;
+@(posedge clk)in=0;
+@(posedge clk)in=1;
+@(posedge clk)in=0;
+#50; $finish;
+end
+endmodule
+
+
+
+
+
+
+
+
