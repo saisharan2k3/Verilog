c19f7d9bdeef9fe5c4fc843d27a09e8945933f4b
saisharan2k3 <saisharan2k3@gmail.com>
2026-03-10 12:18:33 +0530

    Create 4bit_Up_Odd&Even_Design.v

---
+++ Sequential/Counters/4bit_Up/Asynchronous/4bit_Up_Odd&Even_Design.v
@@ -0,0 +1,31 @@
+module async_up(
+  input clk,
+  input reset,
+  output reg [3:0]q);
+  always@(posedge clk or posedge reset) begin
+    if(reset)
+      q[0]<=1'b0;
+    else
+      q[0]<=~q[0];
+  end
+  always@(posedge q[0] or posedge reset) begin
+    if(reset)
+      q[1]<=1'b0;
+    else
+      q[1]<=~q[1];
+    end
+  always@(negedge q[1] or posedge reset) begin
+    if(reset)
+      q[2]<=1'b0;
+    else
+      q[2]<=~q[2];
+      end
+  always@(negedge q[2] or posedge reset) begin
+    if(reset)
+      q[3]<=1'b0;
+    else
+      q[3]<=~q[3];
+        end
+endmodule
+
+//Same TestBench apply for this
