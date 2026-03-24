37d7bb411b558a0a55a4e31feb8623f911369399
saisharan2k3 <saisharan2k3@gmail.com>
2026-03-24 10:07:08 +0530

    Create Design.v

---
+++ Sequential/Register/Parallel_Reg/Design.v
@@ -0,0 +1,11 @@
+module parallel_reg(
+  input clk,reset,
+  input [3:0] d,
+  output reg [3:0]q);
+  always@(posedge clk or posedge reset) begin
+    if(!reset) 
+      q<=4'b0000;
+    else
+      q<=d;
+  end
+endmodule
