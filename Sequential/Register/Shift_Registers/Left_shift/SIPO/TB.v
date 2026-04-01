d6a3ab0e85f23c7ad0d10d6febba043d20e8afac
saisharan2k3 <saisharan2k3@gmail.com>
2026-04-01 23:52:05 +0530

    Create TB.v

---
+++ Sequential/Register/Shift_Registers/Left_shift/SIPO/TB.v
@@ -0,0 +1,21 @@
+module tb;
+  reg clk;
+  reg in;
+  wire out;
+  SIPO dut(.clk(clk),.in(in),.out(out));
+  always #5 clk=~clk;
+  initial begin
+    $monitor("time=%0t clk=%0b in=%0b out=%0b",$time,clk,in,out);
+    $dumpfile("SISO.vcd");
+    $dumpvars(0,tb);
+    clk=0;
+    in=0;
+    #10 in=1;
+    #10 in=0;
+    #10 in=1;
+    #10 in=0;
+    #10 in=1;
+    #10 in=0;
+    #60 $finish;
+  end
+endmodule
