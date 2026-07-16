9d91b649c26013aaf4de2f0633d48920a5acefb5
Sai Sharan P <saisharan2k3@gmail.com>
2026-07-16 15:55:04 +0530

    Create TestBench.v

---
+++ Combinational/Binary_to_Gray/TestBench.v
@@ -0,0 +1,35 @@
+module tb;
+  reg [3:0] bi;
+  wire [3:0] gray;
+  integer i;
+  bi_to_gray BI_Gray(.bi(bi),.gray(gray));
+  initial begin
+    $display("TIME\tBI\t   GRAY"); 
+    for (i=0;i<16;i=i+1)
+    begin
+      bi=i;
+      $display("%0t\t    %b\t   %b",$time,bi,gray);
+      #10;
+    end
+  end
+endmodule
+
+
+TIME	  BI	     GRAY
+0	      0000	   xxxx
+10	    0001	   0000
+20	    0010	   0001
+30	    0011	   0011
+40	    0100	   0010
+50	    0101	   0110
+60	    0110	   0111
+70	    0111	   0101
+80	    1000	   0100
+90	    1001	   1100
+100	    1010	   1101
+110	    1011	   1111
+120	    1100	   1110
+130	    1101	   1010
+140	    1110	   1011
+150	    1111	   1001
+Done
