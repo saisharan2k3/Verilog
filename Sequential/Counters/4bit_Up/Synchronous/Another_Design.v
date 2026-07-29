094a64a2e559e674ceb73af4e159902db8284147
Sai Sharan P <saisharan2k3@gmail.com>
2026-07-29 15:39:19 +0530

    Create Another_Design.v

---
+++ Sequential/Counters/4bit_Up/Synchronous/Another_Design.v
@@ -0,0 +1,11 @@
+module sync_counter(
+  input clk,rst,
+  output reg [3:0]q);
+  always@(posedge clk)
+    begin
+      if(rst)
+          q<=4'b0000;
+      else
+        q<=q+1;
+    end
+endmodule
